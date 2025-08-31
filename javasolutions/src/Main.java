07d93eaf6a37be4f0c99a02de124cb27631bf3dd
JaySanSen <183524427+JaySanSen@users.noreply.github.com>
2025-08-31 22:24:08 +0530

    updated file structure

--- javasolutions/src/Main.java
+++ javasolutions/src/Main.java
@@ -1,6 +1,8 @@
+import Strings.LengthOfLastWord;
+
 public class Main {
     public static void main(String[] args) {
-        TwoSum twoSum = new TwoSum();
-        twoSum.twoSum(new int[] { 0, 2, 3, 4, 5, 5, 7 }, 6);
+    LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+    System.out.println(lengthOfLastWord.lengthOfLastWord("    fly me to the    moon    gfm"));
     }
 }
