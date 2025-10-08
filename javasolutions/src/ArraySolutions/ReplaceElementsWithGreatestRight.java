c9c43f9afa194769d33d68a2ca9392529def1925
JaySanSen <183524427+JaySanSen@users.noreply.github.com>
2025-10-08 21:40:30 +0530

    added new solution
    Signed-off-by: JaySanSen <183524427+JaySanSen@users.noreply.github.com>

--- javasolutions/src/ArraySolutions/ReplaceElementsWithGreatestRight.java
+++ javasolutions/src/ArraySolutions/ReplaceElementsWithGreatestRight.java
@@ -1,5 +1,13 @@
 package ArraySolutions;
 
 public class ReplaceElementsWithGreatestRight {
-  
+  public int[] replaceElements(int[] arr) {
+    int[] ans = new int[arr.length];
+    int rightMax = -1;
+    for (int i = arr.length - 1; i >= 0; i--) {
+      ans[i] = rightMax;
+      rightMax = Math.max(rightMax, arr[i]);
+    }
+    return ans;
+  }
 }
