a4213e41f777cdb09ec461f3391dafa0126c5772
JaySanSen <183524427+JaySanSen@users.noreply.github.com>
2025-10-20 05:05:07 +0530

    formatted the questions
    Signed-off-by: JaySanSen <183524427+JaySanSen@users.noreply.github.com>

--- javasolutions/src/Strings/ValidAnagram.java
+++ javasolutions/src/Strings/ValidAnagram.java
@@ -2,19 +2,19 @@ package Strings;
 
 import java.util.HashMap;
 
-//Example 1:
-//Input: s = "anagram", t = "nagaram"
-//Output: true
-//Example 2:
-//Input: s = "rat", t = "car"
-//Output: false
-//Constraints:
-//1 <= s.length, t.length <= 5 * 104
-//s and t consist of lowercase English letters.
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+// Constraints:
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
 
 public class ValidAnagram {
   public boolean isAnagram1(String s, String t) {
-    if(s.length() != t.length()){
+    if (s.length() != t.length()) {
       return false;
     }
     int[] sampleArr = new int[26];
